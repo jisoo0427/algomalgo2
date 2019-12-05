63d5f9aeb38aa16a63548d90054efd8ffb524cd4
jisoo0427 <50692203+jisoo0427@users.noreply.github.com>
2019-12-06 07:28:24 +0900

    new
    .

---
+++ quick.js
@@ -0,0 +1,284 @@
+(function (cjs, an) {
+
+var p; // shortcut to reference prototypes
+var lib={};var ss={};var img={};
+lib.ssMetadata = [];
+
+
+// symbols:
+// helper functions:
+
+function mc_symbol_clone() {
+	var clone = this._cloneProps(new this.constructor(this.mode, this.startPosition, this.loop));
+	clone.gotoAndStop(this.currentFrame);
+	clone.paused = this.paused;
+	clone.framerate = this.framerate;
+	return clone;
+}
+
+function getMCSymbolPrototype(symbol, nominalBounds, frameBounds) {
+	var prototype = cjs.extend(symbol, cjs.MovieClip);
+	prototype.clone = mc_symbol_clone;
+	prototype.nominalBounds = nominalBounds;
+	prototype.frameBounds = frameBounds;
+	return prototype;
+	}
+
+
+(lib.step3_3 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+}).prototype = getMCSymbolPrototype(lib.step3_3, null, null);
+
+
+(lib.step3_2 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+}).prototype = getMCSymbolPrototype(lib.step3_2, null, null);
+
+
+(lib.step2_9 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+}).prototype = getMCSymbolPrototype(lib.step2_9, null, null);
+
+
+(lib.step2_7 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+}).prototype = getMCSymbolPrototype(lib.step2_7, null, null);
+
+
+(lib.step2_5 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+}).prototype = getMCSymbolPrototype(lib.step2_5, null, null);
+
+
+(lib.step2_4 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+}).prototype = getMCSymbolPrototype(lib.step2_4, null, null);
+
+
+(lib.step2_3 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+}).prototype = getMCSymbolPrototype(lib.step2_3, null, null);
+
+
+(lib.step2_2 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+}).prototype = getMCSymbolPrototype(lib.step2_2, null, null);
+
+
+(lib.step1_9 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+}).prototype = getMCSymbolPrototype(lib.step1_9, null, null);
+
+
+(lib.step1_7 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+}).prototype = getMCSymbolPrototype(lib.step1_7, null, null);
+
+
+(lib.step1_6 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+}).prototype = getMCSymbolPrototype(lib.step1_6, null, null);
+
+
+(lib.step1_5 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+}).prototype = getMCSymbolPrototype(lib.step1_5, null, null);
+
+
+(lib.step1_4 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+}).prototype = getMCSymbolPrototype(lib.step1_4, null, null);
+
+
+(lib.step1_3 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+}).prototype = getMCSymbolPrototype(lib.step1_3, null, null);
+
+
+(lib.step1_2 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+}).prototype = getMCSymbolPrototype(lib.step1_2, null, null);
+
+
+(lib.list = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+}).prototype = getMCSymbolPrototype(lib.list, null, null);
+
+
+(lib.btn = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// 레이어_1
+	this.shape = new cjs.Shape();
+	this.shape.graphics.f("#000000").s().p("AhxBxQgvguAAhDQAAhBAvgwQAwgvBBAAQBDAAAuAvQAwAwAABBQAABDgwAuQguAwhDAAQhBAAgwgwg");
+	this.shape.setTransform(16.1,16.1);
+
+	this.timeline.addTween(cjs.Tween.get(this.shape).wait(1));
+
+}).prototype = getMCSymbolPrototype(lib.btn, new cjs.Rectangle(0,0,32.2,32.2), null);
+
+
+// stage content:
+(lib.quick = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// 레이어_1
+	this.instance = new lib.btn();
+	this.instance.parent = this;
+	this.instance.setTransform(1348.8,690.3,1,1,0,0,0,16.1,16.1);
+
+	this.instance_1 = new lib.step3_3();
+	this.instance_1.parent = this;
+	this.instance_1.setTransform(309.5,685.2,1,1,0,0,0,61.5,79);
+
+	this.instance_2 = new lib.step3_2();
+	this.instance_2.parent = this;
+	this.instance_2.setTransform(106,685.2,1,1,0,0,0,62,79);
+
+	this.instance_3 = new lib.step2_9();
+	this.instance_3.parent = this;
+	this.instance_3.setTransform(1260,483.7,1,1,0,0,0,62,79.5);
+
+	this.instance_4 = new lib.step2_7();
+	this.instance_4.parent = this;
+	this.instance_4.setTransform(1023,483.7,1,1,0,0,0,62,79.5);
+
+	this.instance_5 = new lib.step2_5();
+	this.instance_5.parent = this;
+	this.instance_5.setTransform(700,483.7,1,1,0,0,0,62,79.5);
+
+	this.instance_6 = new lib.step2_4();
+	this.instance_6.parent = this;
+	this.instance_6.setTransform(495.5,483.7,1,1,0,0,0,61.5,79.5);
+
+	this.instance_7 = new lib.step2_2();
+	this.instance_7.parent = this;
+	this.instance_7.setTransform(287,483.7,1,1,0,0,0,62,79.5);
+
+	this.instance_8 = new lib.step2_3();
+	this.instance_8.parent = this;
+	this.instance_8.setTransform(163,483.7,1,1,0,0,0,62,79.5);
+
+	this.instance_9 = new lib.step1_9();
+	this.instance_9.parent = this;
+	this.instance_9.setTransform(1198,271.2,1,1,0,0,0,62,79);
+
+	this.instance_10 = new lib.step1_7();
+	this.instance_10.parent = this;
+	this.instance_10.setTransform(1074,271.2,1,1,0,0,0,62,79);
+
+	this.instance_11 = new lib.step1_6();
+	this.instance_11.parent = this;
+	this.instance_11.setTransform(806,271.2,1,1,0,0,0,62,79);
+
+	this.instance_12 = new lib.step1_2();
+	this.instance_12.parent = this;
+	this.instance_12.setTransform(602,271.2,1,1,0,0,0,62,79);
+
+	this.instance_13 = new lib.step1_5();
+	this.instance_13.parent = this;
+	this.instance_13.setTransform(478,271.2,1,1,0,0,0,62,79);
+
+	this.instance_14 = new lib.step1_3();
+	this.instance_14.parent = this;
+	this.instance_14.setTransform(354,271.2,1,1,0,0,0,62,79);
+
+	this.instance_15 = new lib.step1_4();
+	this.instance_15.parent = this;
+	this.instance_15.setTransform(230,271.2,1,1,0,0,0,62,79);
+
+	this.instance_16 = new lib.list();
+	this.instance_16.parent = this;
+	this.instance_16.setTransform(742.5,93,1,1,0,0,0,421.5,79);
+
+	this.shape = new cjs.Shape();
+	this.shape.graphics.f().s("#000000").ss(1,1,1).p("AChAAQAABCgwAwQgvAvhCAAQhCAAgvgvQgvgwAAhCQAAhBAvgwQAvgvBCAAQBCAAAvAvQAwAwAABBg");
+	this.shape.setTransform(1348.8,690.3);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.shape},{t:this.instance_16},{t:this.instance_15},{t:this.instance_14},{t:this.instance_13},{t:this.instance_12},{t:this.instance_11},{t:this.instance_10},{t:this.instance_9},{t:this.instance_8},{t:this.instance_7},{t:this.instance_6},{t:this.instance_5},{t:this.instance_4},{t:this.instance_3},{t:this.instance_2},{t:this.instance_1},{t:this.instance}]}).wait(1));
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(2081.7,1073.2,34.2,34.2);
+// library properties:
+lib.properties = {
+	id: '4086F8E623D7A243AF81E056F4839DF7',
+	width: 1500,
+	height: 800,
+	fps: 24,
+	color: "#FFFFFF",
+	opacity: 1.00,
+	manifest: [],
+	preloads: []
+};
+
+
+
+// bootstrap callback support:
+
+(lib.Stage = function(canvas) {
+	createjs.Stage.call(this, canvas);
+}).prototype = p = new createjs.Stage();
+
+p.setAutoPlay = function(autoPlay) {
+	this.tickEnabled = autoPlay;
+}
+p.play = function() { this.tickEnabled = true; this.getChildAt(0).gotoAndPlay(this.getTimelinePosition()) }
+p.stop = function(ms) { if(ms) this.seek(ms); this.tickEnabled = false; }
+p.seek = function(ms) { this.tickEnabled = true; this.getChildAt(0).gotoAndStop(lib.properties.fps * ms / 1000); }
+p.getDuration = function() { return this.getChildAt(0).totalFrames / lib.properties.fps * 1000; }
+
+p.getTimelinePosition = function() { return this.getChildAt(0).currentFrame / lib.properties.fps * 1000; }
+
+an.bootcompsLoaded = an.bootcompsLoaded || [];
+if(!an.bootstrapListeners) {
+	an.bootstrapListeners=[];
+}
+
+an.bootstrapCallback=function(fnCallback) {
+	an.bootstrapListeners.push(fnCallback);
+	if(an.bootcompsLoaded.length > 0) {
+		for(var i=0; i<an.bootcompsLoaded.length; ++i) {
+			fnCallback(an.bootcompsLoaded[i]);
+		}
+	}
+};
+
+an.compositions = an.compositions || {};
+an.compositions['4086F8E623D7A243AF81E056F4839DF7'] = {
+	getStage: function() { return exportRoot.getStage(); },
+	getLibrary: function() { return lib; },
+	getSpriteSheet: function() { return ss; },
+	getImages: function() { return img; }
+};
+
+an.compositionLoaded = function(id) {
+	an.bootcompsLoaded.push(id);
+	for(var j=0; j<an.bootstrapListeners.length; j++) {
+		an.bootstrapListeners[j](id);
+	}
+}
+
+an.getComposition = function(id) {
+	return an.compositions[id];
+}
+
+
+
+})(createjs = createjs||{}, AdobeAn = AdobeAn||{});
+var createjs, AdobeAn;
